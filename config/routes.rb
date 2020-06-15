80a02dbb35fe6d3df5d40addea784e99d2ba2408
Temmarie <temmar_whyte16@yahoo.com>
2020-06-15 16:05:53 +0100

    finished toy-app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :microposts
   resources :users
-  # root 'users#index'
-  root 'microposts#index'
+  root 'users#index'
+  # root 'microposts#index'
 end
